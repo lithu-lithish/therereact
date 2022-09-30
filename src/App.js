c5786ac0b7d3cc680a03019537365c654b6a8be8
litchy is he <72008587+lithu-lithish@users.noreply.github.com>
2022-10-01 00:45:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Dashboard from "./pages/dashboard/Dashboard";
-import Login from "./pages/login/Login";
+// import Login from "./pages/login/Login";
 import List from "./pages/table/Table";
 import New from "./pages/new/New";
 import Addpack from "./pages/new/addPackage/Addpack";
@@ -14,19 +14,19 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-        <Route path="/">
-            <Route index element={<Login />}  
-            />
-         </Route> 
+//         <Route path="/">
+//             <Route index element={<Login />}  
+//             />
+//          </Route> 
          <Route path="/dashboard">
             <Route index element={<Dashboard />}  
             />
          </Route> 
 
-            <Route path="/registration">
-              <Route index element={<New />}
-              />
-            </Route>
+//             <Route path="/registration">
+//               <Route index element={<New />}
+//               />
+//             </Route>
 
             <Route path="/addpack">
               <Route index element={<Addpack />}
